64f4ed8544113cfa5651fb5fb99c3ff133904ae7
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-20 11:15:54 -0300

    Metodos em Java

---
+++ src/main/java/school/sptech/Metodos.java
@@ -0,0 +1,41 @@
+package school.sptech;
+
+public class Metodos {
+    public static void exibeAsteriscos() {
+        System.out.println("************************");
+    }
+
+    // assinatura do metodo
+    public static void exibeQuadradoAsteriscos() {
+        exibeAsteriscos();
+        System.out.println("***");
+        exibeAsteriscos();
+    }
+
+    public static void exibeQuadradoAsteriscos(String msg) {
+        exibeAsteriscos();
+        System.out.println("***     " + msg + "     ***");
+        exibeAsteriscos();
+    }
+
+    public static Double media(Double n1, Double n2){
+        return (n1 + n2) / 2;
+    }
+
+    public static Double media(Double n1, Double n2, Double n3){
+        return (n1 + n2 + n3) / 3;
+    }
+
+    public static void main(String[] args) {
+        exibeAsteriscos();
+        System.out.println();
+        exibeQuadradoAsteriscos();
+        System.out.println();
+        exibeQuadradoAsteriscos("Bom dia!");
+
+        System.out.println();
+        System.out.println(media(6.8, 7.0));     // média de 2 números
+        System.out.println(media(7.0, 8.5, 9.5)); // média de 3 números
+
+    }
+}
